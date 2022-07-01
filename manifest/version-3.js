83536f526729435933579ac51f0a95bc9ef3294c
Khalil <khalil.abudahab@cgps.group>
2022-07-01 23:12:42 +0100

    Create version-3.js

---
+++ manifest/version-3.js
@@ -0,0 +1,42 @@
+function renameBindings(bindings, map) {
+  for (const binding of bindings) {
+    if (binding.target in map && map[binding.target]) {
+      binding.target = map[binding.target];
+    }
+  }
+}
+
+module.exports = function (doc) {
+  for (const step of doc.transform) {
+    if (step.type === "adaptor") {
+      if (step.adaptor === "csv-file-to-datatable") {
+        step.adaptor = "import-csv-file";
+        renameBindings(
+          step.bindings,
+          {
+            "separator": "delimiter",
+            "newline": null,
+          },
+        );
+      }
+
+      if (step.adaptor === "join-datatables") {
+        renameBindings(
+          step.bindings,
+          {
+            "overwrite": null,
+            "distance": null,
+          },
+        );
+        const columnsBindings = step.bindings.find((x) => x.target === "columns");
+        if (columnsBindings && columnsBindings.type === "value") {
+          columnsBindings.value = columnsBindings.value.map(([ key ]) => key);
+        }
+      }
+    }
+  }
+
+  doc.version = 3;
+
+  return doc;
+};
