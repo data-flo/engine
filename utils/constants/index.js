1483993e3f4344f03c9537bb584d52c303e129c8
Khalil <khalil.abudahab@cgps.group>
2022-07-14 15:40:10 +0100

    Update index.js

--- utils/constants/index.js
+++ utils/constants/index.js
@@ -3,3 +3,5 @@ module.exports.EmptyArray = Object.freeze([]);
 module.exports.EmptyObject = Object.freeze({});
 
 module.exports.EmptyString = "";
+
+module.exports.EmptySet = Object.freeze(new Set());
