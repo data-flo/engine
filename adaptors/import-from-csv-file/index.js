45e4f230c119dbbe170409ced19362d6baa98586
Khalil <khalil.abudahab@cgps.group>
2022-07-15 23:49:43 +0100

    Update index.js

--- adaptors/import-from-csv-file/index.js
+++ adaptors/import-from-csv-file/index.js
@@ -5,20 +5,18 @@ const { Datatable } = require("../../types/datatable");
 module.exports = async function (args) {
   const datatableWriter = await Datatable.create();
 
-  const columns = (
-    args.columns
-      ?
-      args.columns
-      :
-      (headerCells) => headerCells.map((column) => column.trim())
-  );
-
   args.file.getReader({ encoding: args.encoding })
     .pipe(
       parse({
-        trim: args.trim,
+        columns: (
+          args["column names"]
+            ?
+            args["column names"]
+            :
+            (headerCells) => headerCells.map((column) => column.trim())
+        ),
         delimiter: args.delimiter,
-        columns,
+        trim: args.trim,
       })
     )
     .pipe(
