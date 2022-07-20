4f084d92f39549dc904b4ee031af6e103ae4b761
Khalil <khalil.abudahab@cgps.group>
2022-07-20 12:02:57 +0100

    Update test.js

--- adaptors/prepend-to-list/test.js
+++ adaptors/prepend-to-list/test.js
@@ -1,15 +1,21 @@
 const tap = require("../../utils/testing/unit");
+const runAdaptor = require("../../runner/run-adaptor");
 
 const adaptor = require("./index");
 
 tap.test("prepend-to-list adaptor", async () => {
-  tap.test("given a list, it should add a value to the beginning of the list", async (t) => {
-    const output = await adaptor({
-      list: [ "1", "2" ],
-      value: "3",
-    });
+
+  tap.test("given a list, it should add a value to the beginning of the list", async () => {
+    const output = await runAdaptor(
+      adaptor,
+      {
+        list: [ "1", "2" ],
+        value: "3",
+      },
+    );
     const actual = output.list;
     const expected = [ "3", "1", "2" ];
-    t.same(actual, expected);
+    tap.same(actual, expected);
   });
+
 });
