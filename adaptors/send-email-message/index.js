a8cafed99669f3bc6aeb43ea04ca6fd129c6371b
Khalil <khalil.abudahab@cgps.group>
2022-07-20 23:10:29 +0100

    WIP

--- adaptors/send-email-message/index.js
+++ adaptors/send-email-message/index.js
@@ -2,25 +2,25 @@ const nodemailer = require("nodemailer");
 
 module.exports = async function (args) {
   const transporter = nodemailer.createTransport({
-    host: args["stmp host"],
-    port: args["stmp port"],
-    secure: args["stmp secure"],
+    host: args["smtp host"],
+    port: args["smtp port"],
+    secure: args["smtp secure"],
     auth: {
-      user: args["stmp username"],
-      pass: args["stmp password"],
+      user: args["smtp username"],
+      pass: args["smtp password"],
     },
   });
 
   const info = await transporter.sendMail({
     from: args["from address"],
     to: args["to address"],
-    subject: args["subject"],
-    text: args["text"],
+    subject: args.subject,
+    text: args.text,
   });
 
   return {
     "message id": info.messageId,
-    response: info.response,
+    "status code": info.response,
   };
 };
 
