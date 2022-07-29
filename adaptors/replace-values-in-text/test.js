71501417b2c7fcc12627f836f36867d79fb0eb6a
Khalil <khalil.abudahab@cgps.group>
2022-07-30 00:00:05 +0100

    WIP

---
+++ adaptors/replace-values-in-text/test.js
@@ -0,0 +1,31 @@
+const tap = require("../../utils/testing/unit");
+
+const adaptor = require("./index");
+
+tap.test("replace-values-in-text adaptor", async () => {
+
+  tap.test("given two text without separator, it should return the concatenated text", async (t) => {
+    const output = await adaptor({
+      "left": "Hello",
+      "right": "World",
+      "separator": "",
+    });
+    t.ok(output.combination, "adaptor should return combination");
+    const actual = output.combination;
+    const expected = "HelloWorld";
+    t.equal(actual, expected);
+  });
+
+  tap.test("given two text with separator, it should return the concatenated text", async (t) => {
+    const output = await adaptor({
+      "left": "Hello",
+      "right": "World",
+      "separator": " ",
+    });
+    t.ok(output.combination, "adaptor should return combination");
+    const actual = output.combination;
+    const expected = "Hello World";
+    t.equal(actual, expected);
+  });
+
+});
