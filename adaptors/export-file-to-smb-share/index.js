d7a9cc758a2e31587d2db01078101db6bd745738
Khalil <khalil.abudahab@cgps.group>
2023-02-15 22:01:15 +0000

    export-file-to-smb-share

---
+++ adaptors/export-file-to-smb-share/index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const SambaClient = require("samba-client");
+
+const { FileStream } = require("../../types/file");
+const parseSMBSharePath = require("../../utils/file/parse-smb-share-path");
+
+module.exports = async function (args) {
+  const [ shareAddress, remoteFilePath ] = parseSMBSharePath(
+    args["share address"],
+    args["file path"],
+  );
+
+  const client = new SambaClient({
+    address: shareAddress,
+    domain: args.domain,
+    username: args.username,
+    password: args.password,
+    port: args.port,
+  });
+
+  if (!args.overwrite) {
+    if (await client.fileExists(remoteFilePath)) {
+      throw new Error("File Exists");
+    }
+  }
+
+  await client.sendFile(
+    args.file.getSource(),
+    remoteFilePath,
+  );
+
+  return { file: args.file };
+};
+
+module.exports.manifest = require("./manifest");
---
+++ adaptors/export-file-to-smb-share/test.js
@@ -0,0 +1,48 @@
+const tap = require("../../utils/testing/unit");
+
+const runAdaptor = require("../../runner/run-adaptor");
+const adaptor = require("./index");
+const createTmpTextFile = require("../../utils/file/tmp-text");
+const createFile = require("../../types/file");
+
+tap.test("export-file-to-smb-share adaptor", async () => {
+  const testFilePath = await createTmpTextFile(`(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);`);
+
+  tap.test("given a file, it should upload it", async () => {
+    const output = await runAdaptor(
+      adaptor,
+      {
+        "file": createFile(testFilePath),
+        "share address": "\\\\localhost\\projects",
+        "file path": "data\\demo.newick",
+        "port": 8445,
+        "username": "joe",
+        "password": "samba",
+        "overwrite": true,
+      }
+    );
+    tap.ok(output.file, "adaptor should return file");
+    tap.compareFile(
+      output.file.getSource(),
+      "(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);",
+    );
+  });
+
+  tap.test("given an existing file path, it should throw an error", async (t) => {
+    await t.rejects(
+      runAdaptor(
+        adaptor,
+        {
+          "file": createFile(testFilePath),
+          "share address": "\\\\localhost\\projects",
+          "file path": "data\\demo.newick",
+          "port": 8445,
+          "username": "joe",
+          "password": "samba",
+        }
+      ),
+      ("File Exists"),
+    );
+  });
+
+});
