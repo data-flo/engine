6d397ed4326b56bc6636c600d681ba917ddf776f
Khalil <khalil.abudahab@cgps.group>
2023-08-25 09:52:43 +0100

    Update manifest.js

--- adapters/convert-text-to-datatable/manifest.js
+++ adapters/convert-text-to-datatable/manifest.js
@@ -1,9 +1,9 @@
-const { CommonDelimiters } = require("../../enums");
+const { CommonDelimiters, CommonLineEndings } = require("../../enums.js");
 
 module.exports = {
   "description": "Converts delimited text (e.g. comma-separated values) to a datatable.",
   "group": "Transformations",
-  "subgroup": "Data Tables",
+  "subgroup": "Text Manipulation",
   "input": [
     {
       "name": "csv",
@@ -19,6 +19,28 @@ module.exports = {
       "default": ",",
       "ui": { "can-be-one-of": CommonDelimiters },
     },
+    {
+      "name": "newline",
+      "type": "text",
+      "description": "Character used as line separator.\nIf unspecified, defaults to `\\n` (newline).",
+      "required": false,
+      "default": "\n",
+      "ui": { "can-be-one-of": CommonLineEndings },
+    },
+    {
+      "name": "trim",
+      "type": "boolean",
+      "description": "Specifies whether to ignore whitespace characters immediately around the separator.\nIf unspecified, defaults to `True`.",
+      "required": false,
+      "default": true,
+    },
+    {
+      "name": "columns",
+      "type": "list",
+      "description": "List of column names to be added. If unspecified, first row of the file will be used as column names.",
+      "required": false,
+    },
+
   ],
   "output": [
     {
