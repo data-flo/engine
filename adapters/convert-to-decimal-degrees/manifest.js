4e6ef694a8e2e39aae5757b5e8cd0fe10823a49d
Khalil <khalil.abudahab@cgps.group>
2023-09-28 20:34:27 +0100

    WIP

---
+++ adapters/convert-to-decimal-degrees/manifest.js
@@ -0,0 +1,77 @@
+module.exports = {
+  "description": "Converts location names in a datatable column to latitude & longitude.",
+  "group": "Transformations",
+  "subgroup": "Data Tables",
+  "input": [
+    {
+      "name": "data",
+      "type": "datatable",
+      "description": "The datatable containing the column to be converted.",
+      "required": true,
+    },
+    {
+      "name": "location column",
+      "type": "text",
+      "description": "The name of the column containing location names.",
+      "required": true,
+      "ui": { "column-in": "data" },
+    },
+
+    {
+      "name": "latitude column",
+      "type": "text",
+      "description": "The column name for the latitude result.\nIf unspecified, defaults to \"latitude\".",
+      "required": false,
+      "default": "latitude",
+    },
+    {
+      "name": "longitude column",
+      "type": "text",
+      "description": "The column name for the longitude result.\nIf unspecified, defaults to \"longitude\".",
+      "required": false,
+      "default": "longitude",
+    },
+
+    {
+      "name": "geographic area",
+      "type": "text",
+      "description": "Search within a geographic area. This is a hard filter. Results will be returned if they are located within the specified area. A geographic area can be a country (or multiple countries), provided as comma-separated [ISO 3166-1 alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3) country codes in all uppercase. For example, CAN,MEX,USA.",
+      "required": false,
+    },
+
+    {
+      "name": "digits",
+      "type": "number",
+      "description": "The number of digits to appear after the decimal point; should be a value between 0 and 6, inclusive.\nIf unspecified, it defaults to 6.",
+      "required": false,
+      "default": 6,
+    },
+
+    {
+      "name": "type column",
+      "type": "text",
+      "description": "The name of the column to hold the match type (either `building`, `road`, `hamlet`, `village`, `neighbourhood`, `city`, `county`, `postcode`, `partial_postcode`, `terminated_postcode`, `postal_city`, `state_district`, `state`, `region`, `island`, `body_of_water`, `country`, `continent`, `fictitious`, or `unknown`).\nIf unspecified, the type column will not be added.",
+      "required": false,
+    },
+
+    // {
+    //   "name": "api provider",
+    //   "type": "text",
+    //   "description": "An OpenCage Geocoding API key (https://opencagedata.com/api).",
+    //   "required": true,
+    // },
+    // {
+    //   "name": "api key",
+    //   "type": "text",
+    //   "description": "A valid HERE API Key (https://developer.here.com/documentation/identity-access-management/dev_guide/topics/plat-using-apikeys.html).",
+    //   "required": true,
+    // },
+  ],
+  "output": [
+    {
+      "name": "data",
+      "type": "datatable",
+      "description": "A datatable with the latitude & longitude values added.",
+    },
+  ],
+};
