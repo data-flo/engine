ef312c6d68c712d2ecf1ca3752f43a52e1d17ea7
Khalil <khalil.abudahab@cgps.group>
2023-10-02 05:44:54 +0100

    WIP

--- adapters/convert-to-decimal-degrees/index.js
+++ adapters/convert-to-decimal-degrees/index.js
@@ -1,8 +1,3 @@
-const geocoder = require("../../utils/geocoding/opencage-geocoder");
-const formater = require("../../utils/geocoding/opencage-formater");
-
-const cache = require("../../utils/cache");
-
 module.exports = async function (args) {
   await args.data.shouldIncludeColumns(args["location column"]);
   const data = await args.data.transformAsync(
@@ -10,17 +5,16 @@ module.exports = async function (args) {
       if (row[args["location column"]]) {
         const query = row[args["location column"]];
         if (query) {
-          const parts = query.match(/(\d+[\.,]?\d*)\W?([NS]?)\W?(\d+[\.,]?\d*)\W?([EW]?)/i);
-          const [ _, lat, north, long, east ] = parseFloat(parts[0]);
-          let latitude = parseFloat(lat.replace(",", "."));
-          let longitude = parseFloat(long.replace(",", "."));
+          const [ _, lat, north, long, east ] = query.match(/(\d+[\.,]?\d*)\W?([NS]?)\W?(\d+[\.,]?\d*)\W?([EW]?)/i);
+          let latitude = lat.replace(",", ".");
+          let longitude = long.replace(",", ".");
 
           if (north === "S" || north === "s") {
-            latitude *= -1;
+            latitude = `-${latitude}`;
           }
 
           if (east === "W" || east === "w") {
-            longitude *= -1;
+            longitude = `-${longitude}`;
           }
 
           row[args["latitude column"]] = latitude;
