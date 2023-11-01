fb6cad07eadaf8d8b8f614cb307533c24ac24ecb
Khalil <khalil.abudahab@cgps.group>
2023-11-01 15:41:45 +0000

    Update test.js

--- adapters/add-column/test.js
+++ adapters/add-column/test.js
@@ -1,11 +1,13 @@
-const tap = require("../../utils/testing/unit");
-const fs = require("fs");
+const test = require("node:test");
+const assert = require("node:assert");
 
-const adaptor = require("./index");
-const createTmpTextFile = require("../../utils/file/tmp-text");
-const createDatatable = require("../../types/datatable");
+const { compareFile } = require("../../utils/testing/unit.js");
+const createTmpTextFile = require("../../utils/file/tmp-text.js");
+const createDatatable = require("../../types/datatable.js");
 
-tap.test("add-column adaptor", async () => {
+const adaptor = require("./index.js");
+
+test("add-column adaptor", async (t) => {
   const testCsvFilePath = await createTmpTextFile(`"id","Country","empty","date a","date b"
 "Bovine","de",,"Jan 29, 2007","2007-01-28"
 "Gibbon","fr",,,
@@ -15,14 +17,14 @@ tap.test("add-column adaptor", async () => {
 "Mouse","gb",,,
 `);
 
-  tap.test("given a datatable, it should add a column", async (t) => {
+  await t.test("given a datatable, it should add a column", async () => {
     const output = await adaptor({
       "data": createDatatable(testCsvFilePath),
       "column name": "ones",
       "value": "1",
     });
-    t.ok(output.data);
-    tap.compareFile(
+    assert.ok(output.data);
+    compareFile(
       output.data.getSource(),
       `"id","Country","empty","date a","date b","ones"
 "Bovine","de",,"Jan 29, 2007","2007-01-28","1"
@@ -35,13 +37,13 @@ tap.test("add-column adaptor", async () => {
     );
   });
 
-  tap.test("given no value, it should add an empty column", async (t) => {
+  await t.test("given no value, it should add an empty column", async () => {
     const output = await adaptor({
       "data": createDatatable(testCsvFilePath),
       "column name": "ones",
     });
-    t.ok(output.data);
-    tap.compareFile(
+    assert.ok(output.data);
+    compareFile(
       output.data.getSource(),
       `"id","Country","empty","date a","date b","ones"
 "Bovine","de",,"Jan 29, 2007","2007-01-28",
@@ -54,8 +56,8 @@ tap.test("add-column adaptor", async () => {
     );
   });
 
-  tap.test("given an existing column, it should throw an error", async (t) => {
-    await t.rejects(
+  await t.test("given an existing column, it should throw an error", async () => {
+    await assert.rejects(
       adaptor({
         "data": createDatatable(testCsvFilePath),
         "column name": "id",
