f8458b30b9467e8cfba2804f51865832b5b841af
Khalil <khalil.abudahab@cgps.group>
2023-11-02 11:59:36 +0000

    TASK-1778

--- adapters/import-file-from-dropbox/index.js
+++ adapters/import-file-from-dropbox/index.js
@@ -1,16 +1,16 @@
-const { FileStream } = require("../../types/file");
-const getRequestAsStream = require("../../utils/requests/get-as-stream");
+const { FileStream } = require("../../types/file.js");
+const getRequestAsStream = require("../../utils/requests/get-as-stream.js");
 
 module.exports = async function (args) {
-  const validUrl = /^https:\/\/www\.dropbox\.com\/s\/(.+)\/?/i;
+  const validUrl = /^https:\/\/www\.dropbox\.com\//i;
 
   const match = validUrl.exec(args.url);
 
-  if (!match || !match[1]) {
+  if (!match) {
     throw new Error("Invalid Dropbox file URL.");
   }
 
-  const url = `https://www.dropbox.com/s/dl/${match[1]}`;
+  const url = args.url.replace("dl=0", "dl=1");
 
   const stream = await getRequestAsStream(url);
 
