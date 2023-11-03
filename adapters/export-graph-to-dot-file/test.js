673b64e5cbfb26c9e5d12e85cce1eeaea7daea34
Khalil <khalil.abudahab@cgps.group>
2023-11-03 07:48:04 +0000

    TASK-1842

--- adapters/export-graph-to-dot-file/test.js
+++ adapters/export-graph-to-dot-file/test.js
@@ -1,12 +1,15 @@
-const tap = require("../../utils/testing/unit");
+const assert = require("node:assert");
+const test = require("node:test");
 
-const runAdaptor = require("../../runner/run-adaptor");
+const tap = require("../../utils/testing/unit.js");
 
-const adaptor = require("./index");
+const runAdaptor = require("../../runner/run-adaptor.js");
 
-tap.test("export-graph-to-dot-file adaptor", async () => {
+const adaptor = require("./index.js");
 
-  tap.test("given a DOT string columns, it should return a graph", async (t) => {
+test("export-graph-to-dot-file adaptor", async (t) => {
+
+  await test("given a graph, it should return a DOT file", async () => {
     const output = await runAdaptor(
       adaptor,
       {
@@ -26,10 +29,36 @@ tap.test("export-graph-to-dot-file adaptor", async () => {
         },
       },
     );
-    t.ok(output.dot, "adaptor should return dot");
+    assert.ok(output.dot, "adaptor should return dot");
     const actual = output.dot;
     const expected = "graph G { \"A\" [\"label\"=\"A\"]; \"B\" []; \"C\" []; \"D\" []; \"A\" -- \"B\" [\"label\"=\"A to B\"]; \"A\" -- \"C\" []; \"B\" -- \"C\" []; \"C\" -- \"D\" ; }";
-    t.same(actual, expected);
+    assert.equal(actual, expected);
+  });
+
+  await test("given a graph, it should return a DOT file", async () => {
+    const output = await runAdaptor(
+      adaptor,
+      {
+        graph: {
+          nodes: [
+            { id: "A", attributes: { label: "A" } },
+            { id: "B" },
+            { id: "C" },
+            { id: "D" },
+          ],
+          edges: [
+            { id: "edge-0", from: "A", to: "B", direction: "forward", attributes: { label: "A to B" } },
+            { id: "edge-1", from: "A", to: "C", direction: "none", attributes: {} },
+            { id: "edge-2", from: "B", to: "C", attributes: {} },
+            { id: "edge-3", from: "C", to: "D", direction: "none" },
+          ],
+        },
+      },
+    );
+    assert.ok(output.dot, "adaptor should return dot");
+    const actual = output.dot;
+    const expected = "digraph G { \"A\" [\"label\"=\"A\"]; \"B\" []; \"C\" []; \"D\" []; \"A\" -> \"B\" [\"label\"=\"A to B\"]; \"A\" -- \"C\" []; \"B\" -- \"C\" []; \"C\" -- \"D\" ; }";
+    assert.equal(actual, expected);
   });
 
 });
