d10aad4d54fd173d47ec192ba5aeb7a13eb8ada1
Khalil <khalil.abudahab@cgps.group>
2023-11-06 15:03:51 +0000

    Update to Node.js test runner.

--- adapters/calculate-time-difference/test.js
+++ adapters/calculate-time-difference/test.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
-const tap = require("../../utils/testing/unit");
+const test = require("node:test");
+const assert = require("node:assert");
 
 const { compareFile } = require("../../utils/testing/unit.js");
 
-const createTmpTextFile = require("../../utils/file/tmp-text");
-const createDatatable = require("../../types/datatable");
+const createTmpTextFile = require("../../utils/file/tmp-text.js");
+const createDatatable = require("../../types/datatable.js");
 
-const adaptor = require("./index");
+const adaptor = require("./index.js");
 
-tap.test("calculate-time-difference adaptor", async () => {
+test("calculate-time-difference adaptor", async (t) => {
   const testCsvFilePath = await createTmpTextFile(`"id","Country","empty","date a","date b"
 "Bovine","de",,"Jan 29, 2007","2007-01-28"
 "Gibbon","fr",,,
@@ -18,7 +18,7 @@ tap.test("calculate-time-difference adaptor", async () => {
 "Mouse","gb",,,
 `);
 
-  tap.test("given two columns, it should add the differnce in days", async (t) => {
+  await t.test("given two columns, it should add the differnce in days", async () => {
     const output = await adaptor({
       "data": createDatatable(testCsvFilePath),
       "column one": "date a",
@@ -28,7 +28,7 @@ tap.test("calculate-time-difference adaptor", async () => {
       "difference column": "days",
       "difference unit": "days",
     });
-    t.ok(output.data, "adaptor should return data");
+    assert.ok(output.data, "adaptor should return data");
     compareFile(
       output.data.getSource(),
       `"id","Country","empty","date a","date b","days"
@@ -42,7 +42,7 @@ tap.test("calculate-time-difference adaptor", async () => {
     );
   });
 
-  tap.test("given two columns in reverse, it should add the differnce as negative", async (t) => {
+  await t.test("given two columns in reverse, it should add the differnce as negative", async () => {
     const output = await adaptor({
       "data": createDatatable(testCsvFilePath),
       "column one": "date b",
@@ -51,7 +51,7 @@ tap.test("calculate-time-difference adaptor", async () => {
       "difference column": "days",
       "difference unit": "days",
     });
-    t.ok(output.data, "adaptor should return data");
+    assert.ok(output.data, "adaptor should return data");
     compareFile(
       output.data.getSource(),
       `"id","Country","empty","date a","date b","days"
@@ -65,8 +65,8 @@ tap.test("calculate-time-difference adaptor", async () => {
     );
   });
 
-  tap.test("given non-existing as column one, it should throw an error", async (t) => {
-    await t.rejects(
+  await t.test("given non-existing as column one, it should throw an error", async () => {
+    await assert.rejects(
       adaptor({
         "data": createDatatable(testCsvFilePath),
         "column one": "A",
@@ -80,8 +80,8 @@ tap.test("calculate-time-difference adaptor", async () => {
     );
   });
 
-  tap.test("given non-existing as column two, it should throw an error", async (t) => {
-    await t.rejects(
+  await t.test("given non-existing as column two, it should throw an error", async () => {
+    await assert.rejects(
       adaptor({
         "data": createDatatable(testCsvFilePath),
         "column one": "date a",
@@ -95,8 +95,8 @@ tap.test("calculate-time-difference adaptor", async () => {
     );
   });
 
-  tap.test("given an existing column, it should throw an error", async (t) => {
-    await t.rejects(
+  await t.test("given an existing column, it should throw an error", async () => {
+    await assert.rejects(
       adaptor({
         "data": createDatatable(testCsvFilePath),
         "column one": "date a",
@@ -106,7 +106,7 @@ tap.test("calculate-time-difference adaptor", async () => {
         "difference column": "id",
         "difference unit": "days",
       }),
-      ("Datatable already includes a column named id"),
+      new Error("Datatable already includes a column named id"),
     );
   });
 
