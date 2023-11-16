a1bc12b63a7670538e3003c8c26688d022399ee0
Khalil <khalil.abudahab@cgps.group>
2023-11-16 13:44:37 +0000

    Add test for public objects.

--- adapters/import-file-from-s3/test/index.js
+++ adapters/import-file-from-s3/test/index.js
@@ -8,27 +8,41 @@ test("import-file-from-s3 adaptor", async (t) => {
   before(() => dockerComposeUp(path.resolve(__dirname)));
   after(() => dockerComposeDown(path.resolve(__dirname)));
 
-  // await t.test("given a private object with keys, it should download it", async () => {
-  //   const output = await adaptor(
-  //     {
-  //       "url": "http://localhost:9000/test/output",
-  //       "access key": "NNEV9C8ZYRHATNI2MRZ9",
-  //       "secret key": "DgsAUwzoBssqAAa7WxzeBzO4Ty1rloXIFsTZFWXC",
-  //     }
-  //   );
-  //   assert.ok(output.file, "adaptor should return file");
-  //   assert.equal(output.file.name, "output");
-  //   compareFile(
-  //     output.file.getSource(),
-  //     "(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);\n",
-  //   );
-  // });
+  await t.test("given a pubic object with keys, it should download it", async () => {
+    const output = await adaptor(
+      {
+        "url": "http://localhost:9000/public-bucket/output",
+      }
+    );
+    assert.ok(output.file, "adaptor should return file");
+    assert.equal(output.file.name, "output");
+    compareFile(
+      output.file.getSource(),
+      "(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);\n",
+    );
+  });
+
+  await t.test("given a private object with keys, it should download it", async () => {
+    const output = await adaptor(
+      {
+        "url": "http://localhost:9000/private-bucket/output",
+        "access key": "NNEV9C8ZYRHATNI2MRZ9",
+        "secret key": "DgsAUwzoBssqAAa7WxzeBzO4Ty1rloXIFsTZFWXC",
+      }
+    );
+    assert.ok(output.file, "adaptor should return file");
+    assert.equal(output.file.name, "output");
+    compareFile(
+      output.file.getSource(),
+      "(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);\n",
+    );
+  });
 
   await t.test("given a private object without keys, it should not download it", async () => {
     await assert.rejects(
       adaptor(
         {
-          "url": "http://localhost:9000/test/output",
+          "url": "http://localhost:9000/private-bucket/output",
         }
       ),
       new Error("Request failed with status code 403: Forbidden"),
