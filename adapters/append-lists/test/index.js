af3558d8effb3ec39bad801c233212b52d248cc9
Khalil <khalil.abudahab@cgps.group>
2023-11-21 06:11:11 +0000

    TASK-2064

--- adapters/append-lists/test/index.js
+++ adapters/append-lists/test/index.js
@@ -1,10 +1,11 @@
-/* eslint-disable quotes */
-const tap = require("../../utils/testing/unit");
+const test = require("node:test");
+const assert = require("node:assert");
 
-const runAdaptor = require("../../runner/run-adaptor");
-const adaptor = require("./index");
+const runAdaptor = require("../../../runner/run-adaptor.js");
 
-await t.test("append-lists adaptor", async () => {
+const adaptor = require("../index.js");
+
+test("append-lists adaptor", async (t) => {
 
   await t.test("given two lists, it should return a merged list", async () => {
     const output = await runAdaptor(
