c486ead3330a3763af3e1797bca93140f6a310e5
Khalil <khalil.abudahab@cgps.group>
2023-11-21 20:47:35 +0000

    TASK-2086

--- utils/file/tmp-path.js
+++ utils/file/tmp-path.js
@@ -1,9 +1,11 @@
 const tmp = require("tmp-promise");
+const touch = require("touch");
 
 const { EmptyObject } = require("../constants/index.js");
 
 module.exports = async function (options = EmptyObject) {
   const path = await tmp.tmpName(options);
+  await touch(path);
   // const { path } = await tmp.file({
   //   discardDescriptor: true,
   //   detachDescriptor: true,
