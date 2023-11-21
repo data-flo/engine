f8146b666ab80d177ff1337732d556ba3ac873ed
Khalil <khalil.abudahab@cgps.group>
2023-11-21 18:20:40 +0000

    Update tmp-path.js

--- utils/file/tmp-path.js
+++ utils/file/tmp-path.js
@@ -3,11 +3,12 @@ const tmp = require("tmp-promise");
 const { EmptyObject } = require("../constants/index.js");
 
 module.exports = async function (options = EmptyObject) {
-  const { path } = await tmp.file({
-    discardDescriptor: true,
-    detachDescriptor: true,
-    keep: true,
-    ...options,
-  });
+  const path = await tmp.tmpName(options);
+  // const { path } = await tmp.file({
+  //   discardDescriptor: true,
+  //   detachDescriptor: true,
+  //   keep: true,
+  //   ...options,
+  // });
   return path;
 };
