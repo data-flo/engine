8f7d8b1b0e1aa1af88f0a22c54b3d29afd7a6364
Khalil <khalil.abudahab@cgps.group>
2023-11-21 21:38:30 +0000

    TASK-2028

--- adapters/import-from-epicollect-project/test/index.js
+++ adapters/import-from-epicollect-project/test/index.js
@@ -1,10 +1,12 @@
-/* eslint-disable quotes */
-const tap = require("../../utils/testing/unit");
+const test = require("node:test");
+const assert = require("node:assert");
 
-const runAdaptor = require("../../runner/run-adaptor");
-const adaptor = require("./index");
+const { compareFile } = require("../../../utils/testing/unit.js");
+const runAdaptor = require("../../../runner/run-adaptor.js");
 
-await t.test("import-epicollect-project", async () => {
+const adaptor = require("../index.js");
+
+test("import-epicollect-project", async (t) => {
 
   await t.test("given a csv text, it should return a datatable", async () => {
     const output = await runAdaptor(
