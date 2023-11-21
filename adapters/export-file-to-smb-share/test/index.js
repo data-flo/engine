854ff64f94765f328679eb940f26158b5a3ce31b
Khalil <khalil.abudahab@cgps.group>
2023-11-21 15:16:36 +0000

    TASK-2082

--- adapters/export-file-to-smb-share/test/index.js
+++ adapters/export-file-to-smb-share/test/index.js
@@ -1,11 +1,17 @@
-const tap = require("../../utils/testing/unit");
+const assert = require("node:assert");
+const path = require("node:path");
+const test = require("node:test");
 
-const runAdaptor = require("../../runner/run-adaptor");
-const adaptor = require("./index");
-const createTmpTextFile = require("../../utils/file/tmp-text");
-const createFile = require("../../types/file");
+const { compareFile, setupServices } = require("../../../utils/testing/unit.js");
+const createTmpTextFile = require("../../../utils/file/tmp-text.js");
+const createFile = require("../../../types/file.js");
+const runAdaptor = require("../../../runner/run-adaptor.js");
 
-await t.test("export-file-to-smb-share adaptor", async () => {
+const adaptor = require("../index.js");
+
+test("export-file-to-smb-share adaptor", async (t) => {
+  setupServices(path.resolve(__dirname));
+
   const testFilePath = await createTmpTextFile(`(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);`);
 
   await t.test("given a file, it should upload it", async () => {
@@ -13,8 +19,8 @@ await t.test("export-file-to-smb-share adaptor", async () => {
       adaptor,
       {
         "file": createFile(testFilePath),
-        "share address": "\\\\localhost\\projects",
-        "file path": "data\\demo.newick",
+        "share address": "\\\\localhost\\share",
+        "file path": "demo.nwk",
         "port": 8445,
         "username": "joe",
         "password": "samba",
@@ -23,25 +29,25 @@ await t.test("export-file-to-smb-share adaptor", async () => {
     );
     assert.ok(output.file, "adaptor should return file");
     compareFile(
-      output.file.getSource(),
+      path.resolve(__dirname, "share", "demo.nwk"),
       "(Bovine:0.69395,(Gibbon:0.0,(Orangutan:0.0,(Gorilla:0.0,(Chimp:0.0,Human:0.0)123:0.0)test:0.06124):0.0):0.54939,Mouse:1.21460);",
     );
   });
 
-  await t.test("given an existing file path, it should throw an error", async (t) => {
-    await t.rejects(
+  await t.test("given an existing file path, it should throw an error", async () => {
+    await assert.rejects(
       runAdaptor(
         adaptor,
         {
           "file": createFile(testFilePath),
-          "share address": "\\\\localhost\\projects",
-          "file path": "data\\demo.newick",
+          "share address": "\\\\localhost\\share",
+          "file path": "demo.nwk",
           "port": 8445,
           "username": "joe",
           "password": "samba",
         }
       ),
-      ("File Exists"),
+      new Error("File Exists"),
     );
   });
 
