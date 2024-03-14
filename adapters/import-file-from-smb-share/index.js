b7a186b5db2059a2b9f7207d44e808ab932f1e9d
khalil-abudahab <khalil.abudahab@cgps.group>
2024-03-14 10:18:49 +0000

    Update index.js

--- adapters/import-file-from-smb-share/index.js
+++ adapters/import-file-from-smb-share/index.js
@@ -4,43 +4,57 @@ const SambaClient = require("samba-client");
 const { FileStream } = require("../../types/file.js");
 const lastElement = require("../../utils/arrays/last-element.js");
 
-module.exports = async function (args) {
-  let shareAddress = args["share address"];
+const cleanErrorMessage = require("../../utils/file/clean-smb-error.js");
+
+function rewriteShareAddress(shareAddress) {
   if (/^smb:\/\//i.test(shareAddress)) {
-    shareAddress = shareAddress.substr(4);
-  }
-  shareAddress = shareAddress.replace(/\\/g, "/");
-
-  const client = new SambaClient({
-    address: shareAddress,
-    domain: args.domain,
-    username: args.username,
-    password: args.password,
-    port: args.port,
-    // maxProtocol: 'SMB3', // not required
-    // maskCmd: true, // not required, defaults to false
-  });
-
-  let remoteFilePath = args["file path"];
-  if (remoteFilePath.startsWith(args["share address"])) {
-    remoteFilePath = remoteFilePath.substr(args["share address"].length);
-  }
-  remoteFilePath = remoteFilePath.replace(/\\/g, "/");
-  if (remoteFilePath.startsWith("/")) {
-    remoteFilePath = remoteFilePath.substr(1);
+    return shareAddress.substr(4);
   }
 
-  const name = lastElement(remoteFilePath.split("/")) || "file";
+  return shareAddress.replace(/\\/g, "/");
+}
 
-  const file = await FileStream.createEmpty({ name });
+module.exports = async function (args) {
+  const shareAddress = rewriteShareAddress(args["share address"]);
 
-  await client.getFile(
-    remoteFilePath,
-    path.basename(file.getSource()),
-    path.dirname(file.getSource()),
-  );
+  try {
+    const client = new SambaClient({
+      address: shareAddress,
+      domain: args.domain,
+      username: args.username,
+      password: args.password,
+      port: args.port,
+      // maxProtocol: 'SMB3', // not required
+      // maskCmd: true, // not required, defaults to false
+    });
 
-  return { file };
+    let remoteFilePath = args["file path"];
+    if (remoteFilePath.startsWith(args["share address"])) {
+      remoteFilePath = remoteFilePath.substr(args["share address"].length);
+    }
+    remoteFilePath = remoteFilePath.replace(/\\/g, "/");
+    if (remoteFilePath.startsWith("/")) {
+      remoteFilePath = remoteFilePath.substr(1);
+    }
+
+    const name = lastElement(remoteFilePath.split("/")) || "file";
+
+    const file = await FileStream.createEmpty({ name });
+
+    await client.getFile(
+      remoteFilePath,
+      path.basename(file.getSource()),
+      path.dirname(file.getSource()),
+    );
+
+    return { file };
+  }
+  catch (err) {
+    cleanErrorMessage(err, args);
+    throw err;
+  }
 };
 
 module.exports.manifest = require("./manifest.js");
+
+module.exports.cleanErrorMessage = cleanErrorMessage;
