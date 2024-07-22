46ee2b231b24ab9ad0a8b80b03cda76086b0cb4f
pranit1 <pspranits10494@gmail.com>
2024-07-22 08:23:32 +0100

    Fixed bugs

--- adapters/geocoding/test/index.js
+++ adapters/geocoding/test/index.js
@@ -53,3 +53,49 @@ test("forward-geocoding adaptor", async (t) => {
   });
 
 });
+
+test("forward-geocoding adaptor with empty first row", async (t) => {
+  assert.ok(process.env.OPENCAGE_API_KEY, "OPENCAGE_API_KEY is missing from env");
+
+  const testCsvFilePath = await createTmpTextFile(`"location"
+""
+"1330 Middle Avenue, Menlo Park, CA 94025, United States of America"
+"United Kingdom"
+"Viet Nam"
+"London"
+"Sawston"
+"CB22 3DQ"
+"Big Ben"
+`);
+  const data = createDatatable(testCsvFilePath);
+
+  await t.test("given a location column, it should return latitude and longitude", async (t) => {
+    const output = await runAdaptor(
+      adaptor,
+      {
+        "api key": process.env.OPENCAGE_API_KEY,
+        "data": data,
+        "location column": "location",
+        "latitude column": "latitude",
+        "longitude column": "longitude",
+        "place type column": "type",
+        "decimal places": 4,
+      },
+    );
+    assert.ok(output.data);
+    compareFile(
+      output.data.getSource(),
+      `"location","latitude","longitude","type"
+,,,
+"1330 Middle Avenue, Menlo Park, CA 94025, United States of America","37.4397","-122.1865","building"
+"United Kingdom","54.7024","-3.2766","country"
+"Viet Nam","15.9267","107.9651","country"
+"London","51.5074","-0.1278","city"
+"Sawston","52.1252","0.1693","village"
+"CB22 3DQ","52.1270","0.1716","postcode"
+"Big Ben","51.5007","-0.1246","attraction"
+`
+    );
+  });
+
+});
--- utils/date/to-string.js
+++ utils/date/to-string.js
@@ -1,16 +1,15 @@
-const { format, isValid } = require("date-fns");
+const { isValid } = require("date-fns");
 const locale = require("date-fns/locale");
 const { formatInTimeZone } = require("date-fns-tz");
 
 const standardiseFormatString = require("./standardise-format-string");
 
-module.exports = function (dateValue, formatString, localeString, timezoneCode) {
+module.exports = function (dateValue, formatString, localeString, timeZone) {
   if (isValid(dateValue)) {
     const cleanFormatString = standardiseFormatString(formatString);
-
     return formatInTimeZone(
-      dateValue,
-      timezoneCode || "UTC",
+      new Date(dateValue),
+      timeZone || "UTC",
       cleanFormatString || "yyyy-MM-dd'T'HH:mm:ssxxx",
       localeString ? { locale: locale[localeString.replace("-", "")] } : undefined,
     );
