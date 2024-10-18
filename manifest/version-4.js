b8d5890b63525d45d50860a90a964e557fca3cb1
Khalil <khalil.abudahab@cgps.group>
2024-10-18 16:54:06 +0100

    TASK-3771

--- manifest/version-4.js
+++ manifest/version-4.js
@@ -14,22 +14,9 @@ module.exports = function (doc) {
       }
     }
 
-    if (step.adaptor === "create-list-from-datatable") {
-      const binding = step.binding?.find((x) => x.binding.target === "column name");
-      if (binding) {
-        if (binding.type === "value") {
-          binding.target = "column names";
-          binding.value = [ binding.value ];
-        }
-        else {
-          step.binding.splice(step.binding.indoxOf(binding), 1);
-        }
-      }
-    }
-
     if (step.adaptor === "map-column-values") {
-      const originalColumnBinding = step.binding?.find((x) => x.binding.target === "original column");
-      const newColumnBinding = step.binding?.find((x) => x.binding.target === "new column");
+      const originalColumnBinding = step.binding?.find((x) => x.target === "original column");
+      const newColumnBinding = step.binding?.find((x) => x.target === "new column");
       if (originalColumnBinding) {
         if (originalColumnBinding.type === "value") {
           originalColumnBinding.target = "columns";
@@ -40,11 +27,11 @@ module.exports = function (doc) {
           originalColumnBinding.value = [ entry ];
         }
         else {
-          step.binding.splice(step.binding.indoxOf(originalColumnBinding), 1);
+          step.binding.splice(step.binding.indexOf(originalColumnBinding), 1);
         }
       }
       if (newColumnBinding) {
-        step.binding.splice(step.binding.indoxOf(newColumnBinding), 1);
+        step.binding.splice(step.binding.indexOf(newColumnBinding), 1);
       }
     }
   }
