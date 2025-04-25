e160699fa3271ff49973718a359ba712bad46143
Khalil <khalil.abudahab@cgps.group>
2025-04-25 09:50:49 +0100

    Add debug messages.

--- adapters/export-to-microreact-project/utils/get-file-url.js
+++ adapters/export-to-microreact-project/utils/get-file-url.js
@@ -1,7 +1,11 @@
+const debug = require("cgps-stdlib/logger/debug.js");
+
 const storeFile = require("./store-file.js");
 
 module.exports = async function getFileUrl(apiUrl, accessToken, file, url) {
   if (file) {
+    debug("Saving file via Microreact API...");
+
     const svaedUrl = await storeFile(
       apiUrl,
       accessToken,
--- adapters/export-to-microreact-project/utils/store-file.js
+++ adapters/export-to-microreact-project/utils/store-file.js
@@ -1,7 +1,49 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const debug = require("cgps-stdlib/logger/debug.js");
+
 const apiRequest = require("./api-request.js");
 
-module.exports = function storeFile(apiUrl, accessToken, fileInput) {
-  return apiRequest({
+/**
+ * Calculates the SHA-1 hash of a file
+ * @param {string} filePath - Path to the file
+ * @returns {Promise<string>} SHA-1 hash of the file
+ */
+async function calculateFileHash(filePath) {
+  return new Promise((resolve, reject) => {
+    const shasum = crypto.createHash("sha1");
+    const stream = fs.createReadStream(filePath);
+
+    stream.on("data", (data) => {
+      shasum.update(data);
+    });
+
+    stream.on("error", (error) => {
+      reject(error);
+    });
+
+    stream.on("end", () => {
+      const hash = shasum.digest("hex");
+      resolve(hash);
+    });
+  });
+}
+
+module.exports = async function storeFile(apiUrl, accessToken, fileInput) {
+  debug("Uploading file to Microreact API...", { path: fileInput.path });
+
+  // Calculate hash before uploading
+  if (fileInput.path) {
+    try {
+      const fileHash = await calculateFileHash(fileInput.path);
+      debug("File SHA-1 hash:", fileHash);
+    }
+    catch (error) {
+      debug("Error calculating file hash:", error);
+    }
+  }
+
+  const res = await apiRequest({
     method: "post",
     baseURL: apiUrl,
     url: "/api/files/store/",
@@ -12,4 +54,6 @@ module.exports = function storeFile(apiUrl, accessToken, fileInput) {
     maxContentLength: Infinity,
     maxBodyLength: Infinity,
   });
+  debug("File uploaded to Microreact API", res);
+  return res;
 };
