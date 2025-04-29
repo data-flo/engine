4191b86b99f29a27353539697029c17478a636e9
Khalil <khalil.abudahab@cgps.group>
2025-04-29 11:01:58 +0100

    Use superagent

--- adapters/export-to-microreact-project/utils/store-file.js
+++ adapters/export-to-microreact-project/utils/store-file.js
@@ -1,8 +1,11 @@
 const crypto = require("crypto");
 const fs = require("fs");
 const debug = require("cgps-stdlib/logger/debug.js");
-
-const apiRequest = require("./api-request.js");
+// const { request } = require("undici");
+// const { Curl } = require("node-libcurl");
+// const { fetch } = require("undici");
+const superagent = require("superagent");
+// const ky = require("ky");
 
 /**
  * Calculates the SHA-1 hash of a file
@@ -29,6 +32,56 @@ async function calculateFileHash(filePath) {
   });
 }
 
+// async function sendFileAsRequestBody(filePath, url, accessToken, method = "POST") {
+//   // Create a new Curl instance
+//   const curl = new Curl();
+
+//   try {
+//     // Read the file content
+//     const fileContent = fs.readFileSync(filePath);
+
+//     // Get file information for content-type header
+//     const fileStats = fs.statSync(filePath);
+
+//     // Configure curl request
+//     curl.setOpt(Curl.option.URL, url);
+//     curl.setOpt(Curl.option.CUSTOMREQUEST, method); // HTTP method
+//     curl.setOpt(Curl.option.POSTFIELDS, fileContent); // Set file content as body
+//     curl.setOpt(Curl.option.HTTPHEADER, [
+//       `Content-Type: application/octet-stream`, // Use appropriate content-type
+//       `Content-Length: ${fileStats.size}`,
+//       `access-token: ${accessToken}`,
+//     ]);
+
+//     // Optional: Verbose output for debugging
+//     // curl.setOpt(Curl.option.VERBOSE, true);
+
+//     // Return a promise for the request
+//     return new Promise((resolve, reject) => {
+//       curl.on("end", function (statusCode, data, headers) {
+//         console.log(`Status code: ${statusCode}`);
+//         console.log(`Response: ${data}`);
+
+//         resolve({ statusCode, data, headers });
+//         this.close();
+//       });
+
+//       curl.on("error", function (error) {
+//         console.error("Error:", error);
+//         reject(error);
+//         this.close();
+//       });
+
+//       // Execute the request
+//       curl.perform();
+//     });
+//   }
+//   catch (error) {
+//     curl.close();
+//     throw error;
+//   }
+// }
+
 module.exports = async function storeFile(apiUrl, accessToken, fileInput) {
   debug("Uploading file to Microreact API...", { path: fileInput.path });
 
@@ -43,17 +96,57 @@ module.exports = async function storeFile(apiUrl, accessToken, fileInput) {
     }
   }
 
-  const res = await apiRequest({
-    method: "post",
-    baseURL: apiUrl,
-    url: "/api/files/store/",
-    data: fileInput,
-    headers: {
-      "access-token": accessToken,
-    },
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-  });
-  debug("File uploaded to Microreact API", res);
-  return res;
+  // const {
+  //   body,
+  //   statusCode,
+  //   headers,
+  // } = await fetch(
+  //   `${apiUrl}api/files/store/`,
+  //   {
+  //     method: "POST",
+  //     // dispatcher: agent,
+  //     headers: { "access-token": accessToken },
+  //     body: fs.createReadStream(fileInput.path),
+  //     duplex: "half", // Add this line to fix the error
+  //   },
+  // );
+  // const res = await body.json();
+  // const res = await sendFileAsRequestBody(
+  //   fileInput.path,
+  //   `${apiUrl}api/files/store/`,
+  //   accessToken,
+  // );
+  // console.error(`${apiUrl}api/files/store/`, { statusCode, headers });
+
+  const buffer = await fs.promises.readFile(fileInput.path);
+
+  const res = await superagent.post(`${apiUrl}api/files/store/`)
+    .set("access-token", accessToken)
+    // .attach("file", fileInput.path)
+    // .set("Content-Type", "application/octet-stream") // or appropriate content-type
+    // .send(fs.readFileSync(fileInput.path)) // you can also stream with `.pipe()`
+    .send(buffer)
+    // .then((r) => {
+    //   debug("File uploaded successfully", r.status);
+    //   return r.body;
+    // })
+    .catch((err) => {
+      debug("Error uploading file", err);
+      throw err;
+    });
+  const json = res.body;
+
+  // const res = await ky.default.post(
+  //   `${apiUrl}api/files/store/`,
+  //   {
+  //     method: "POST",
+  //     headers: { "access-token": accessToken },
+  //     // body: fs.createReadStream(fileInput.path),
+  //   }
+  // )
+  //   .json();
+
+  debug("File uploaded to Microreact API", json);
+
+  return json;
 };
